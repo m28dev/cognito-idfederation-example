4fd467e7ece16a5902c124cf157a4d935eaee912
m28dev <29790650+m28dev@users.noreply.github.com>
2020-10-06 00:17:40 +0900

    [Add] JWT（アクセストークン）の検証を実装

--- backend-example/verify.js
+++ backend-example/verify.js
@@ -2,14 +2,16 @@
 // サンプル通りに実装しようと思ったがJWKから鍵を生成するライブラリもAuth0にあったので、そっちにした
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
+require('dotenv').config();
 
 // Cognito settings
 const cognitoRegion = process.env.COGNITO_REGION;
 const cognitoUserPoolID = process.env.COGNITO_USER_POOL_ID;
+const issuer = `https://cognito-idp.${cognitoRegion}.amazonaws.com/${cognitoUserPoolID}`;
 
 // JWK defines
 const client = jwksClient({
-    jwksUri: `https://cognito-idp.${cognitoRegion}.amazonaws.com/${cognitoUserPoolID}/.well-known/jwks.json`
+    jwksUri: `${issuer}/.well-known/jwks.json`
 });
 const getKey = (header, callback) => {
     client.getSigningKey(header.kid, (err, key) => {
@@ -22,9 +24,20 @@ const getKey = (header, callback) => {
 let token = '';
 
 // verify
-jwt.verify(token, getKey, {}, (err, decoded) => {
+// 署名検証、issの検証、および有効期限チェックを実施
+const options = {
+    algorithm: ['RS256'],
+    issuer,
+    ignoreExpiration: false
+};
+jwt.verify(token, getKey, options, (err, decoded) => {
     if (err) {
         console.error(err);
+        return;
+    }
+    // バックエンドではアクセストークンで認可制御する
+    if(decoded.token_use != 'access') {
+        console.error('Only accept access tokens.');
         return;
     }
     console.log(decoded);
